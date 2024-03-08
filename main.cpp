27bdddce1b5f08b73ef1f8ffc1f58be23cc222e4
Aditi <106754065+adiy-13@users.noreply.github.com>
2024-03-09 00:52:53 +0530

    main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,6 @@
 
 int main()
 {
-    std::cout << "Hello, world , This is PES1UG21CSO38 ! Im making changes now to check if build updates" << std::endl;
+    std::cout << "Hello, world" << std::endl;
     return 0;
 }
